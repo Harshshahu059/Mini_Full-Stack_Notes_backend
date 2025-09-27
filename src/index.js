c58575bd1648da10866aeb3be231ae79eed3dea2
Harshshahu059 <harshshahu059>
2025-09-27 16:58:52 +0530

    Fixing cors error

--- src/index.js
+++ src/index.js
@@ -16,17 +16,21 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.Cors,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true
   }
 });
 setSocket(io);
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.Cors,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/notes", noteRoutes); // pass io for realtime
+app.use("/api/notes", noteRoutes);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
